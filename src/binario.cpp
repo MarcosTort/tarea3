b33f55df859bb18048882da68cb715468be40406
MarcosTort <72978798+MarcosTort@users.noreply.github.com>
2021-04-27 23:34:49 -0300

    Update binario.cpp

--- src/binario.cpp
+++ src/binario.cpp
@@ -50,9 +50,9 @@ TBinario insertarEnBinario(TInfo i, TBinario b){
   assert(esVacioBinario(buscarSubarbol(natInfo(i), b)));
 
   if(!esVacioBinario(b)){
-    if(natInfo(i)<natInfo(b->dato)){b = insertarEnBinario(i, b->izq);}
+    if(natInfo(i)>natInfo(b->dato)){b = insertarEnBinario(i, b->izq);}
     else 
-    if(natInfo(i)>natInfo(b->dato)){b = insertarEnBinario(i, b->der);}
+    if(natInfo(i)<natInfo(b->dato)){b = insertarEnBinario(i, b->der);}
   }
   else{
     b = new _rep_binario;

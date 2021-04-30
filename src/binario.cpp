dbec21c70c93b93689733381ac0a02bc499a4520
MarcosTort <72978798+MarcosTort@users.noreply.github.com>
2021-04-30 09:23:13 -0300

    Update binario.cpp

--- src/binario.cpp
+++ src/binario.cpp
@@ -291,9 +291,9 @@ double sumaUltimosPositivos(nat i, TBinario b){
   if(!esVacioBinario(b)){
     res = res + sumaUltimosPositivos(i, b->der);
     if (i>0 && realInfo(raiz(b)) >0){
-      //printf("La posicion es: ");
-      //printf("%i", natInfo(raiz(b)));
-      //printf("\n");
+      printf("La posicion es: ");
+      printf("%i", natInfo(raiz(b)));
+      printf("\n");
       res = res + realInfo(raiz(b));
       i--;
       }
